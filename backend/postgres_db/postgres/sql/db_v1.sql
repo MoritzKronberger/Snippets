5b57c8f8de15ebb6853786ece5f2068a117758bf
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2021-05-25 09:52:49 +0200

    Add new db with table user

---
+++ backend/postgres_db/postgres/sql/db_v1.sql
@@ -0,0 +1,80 @@
+BEGIN;
+
+/* CLEANUP */
+
+DROP TABLE IF EXISTS user         CASCADE;
+DROP TABLE IF EXISTS post         CASCADE;
+DROP TABLE IF EXISTS comment      CASCADE;
+DROP TABLE IF EXISTS user_like    CASCADE;
+DROP TABLE IF EXISTS e_language   CASCADE;
+DROP TABLE IF EXISTS e_category   CASCADE;
+DROP TABLE IF EXISTS has_category CASCADE;
+
+
+/* Create Domains */
+
+/* from https://gitlab.multimedia.hs-augsburg.de/kowa/wk_account_postgres_01a.git */
+CREATE DOMAIN D_UNTAINTED
+AS VARCHAR (25) CHECK (value !~ '[<>"'';]|--|/\*');
+
+
+/* Create Tables */
+
+/* from https://gitlab.multimedia.hs-augsburg.de/kowa/wk_account_postgres_01a.git */
+CREATE TABLE user 
+(id                     UUID                PRIMARY KEY DEFAULT gen_random_uudi(),
+ username               D_UNTAINTED         UNIQUE NOT NULL,
+ password               VARCHAR (20)        NOT NULL,
+ profile_picture        BYTEA
+);
+
+
+/* Create Triggers and Functions */
+
+/* from https://gitlab.multimedia.hs-augsburg.de/kowa/wk_account_postgres_01a.git */
+CREATE FUNCTION hash_password_function() RETURNS TRIGGER AS
+$_plpgsql_$
+    BEGIN
+        IF( TG_OP = 'INSERT' OR (TG_OP = 'UPDATE' AND NEW.password <> OLD-password))
+            THEN NEW:password = crypt(NEW:password, gen_salt('bf',12));
+        END IF;
+
+        RETURN NEW;
+    END;
+$_plpgsql_$
+LANGUAGE plpgsql;
+
+/* from https://gitlab.multimedia.hs-augsburg.de/kowa/wk_account_postgres_01a.git */
+CREATE TRIGGER hash_password_trigger
+BEFORE INSERT OR UPDATE
+ON user
+FOR EACH ROW
+    EXECUTE PROCEDURE hash_password_function()
+;
+
+CREATE FUNCTION check_password(usrname VARCHAR, pw VARCHAR) RETURNS BOOLEAN AS
+$_SQL_$
+    SELECT EXISTS
+        (SELECT *
+         FROM user
+         WHERE usrname = username AND password = crypt(pw, password)
+        );
+$_SQL_$
+LANGUAGE SQL
+IMMUTABLE
+RETURNS NULL ON NULL INPUT;
+
+COMMIT;
+
+
+/* Insert Testdata */
+
+BEGIN;
+
+INSERT INTO user (username, password)
+VALUES
+('tinykoala648', 'raistlin'),
+('heavyduck567', 'santafe'),
+('smallladybug804', 'aztnm');
+
+COMMIT;
