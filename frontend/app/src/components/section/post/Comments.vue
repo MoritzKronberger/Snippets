b13473fbc234c8683d924910830c3a09b26f2218
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2021-05-26 09:31:38 +0200

    comments component

---
+++ frontend/app/src/components/section/post/Comments.vue
@@ -0,0 +1,18 @@
+<template>
+    <body>
+        <p class="content comment">{{comments.message}}</p>
+    </body>
+</template>
+
+<script>
+import { mapFields  } from 'vuex-map-fields'
+import { mapState } from 'vuex'
+export default {
+    name: 'Comments',
+    props:{comments: Object},
+    computed:
+    {   ...mapState({posts: 'post_object'}),  
+        ...mapFields ([ 'new_comment']),
+    },
+}
+</script>
