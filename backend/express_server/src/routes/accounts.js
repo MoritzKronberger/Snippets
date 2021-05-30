aabd0c10e3dada305f9cf0005946adc648423538
jelllea <lea.jell@hs-augsburg.de>
2021-05-30 12:41:15 +0200

    reformatted folder routes

--- backend/express_server/src/routes/accounts.js
+++ backend/express_server/src/routes/accounts.js
@@ -1,7 +1,9 @@
-import Router   from 'express-promise-router'
-import { getAccounts } from '../db/accounts.js';
+import Router from "express-promise-router";
+import { getAccounts } from "../db/accounts.js";
 
 const accountRouter = Router();
-accountRouter.get("/getAccounts", (req, res) => getAccounts().then((accounts) => res.status(200).json(accounts)));
+accountRouter.get("/getAccounts", (req, res) =>
+  getAccounts().then((accounts) => res.status(200).json(accounts))
+);
 
 export default accountRouter;
