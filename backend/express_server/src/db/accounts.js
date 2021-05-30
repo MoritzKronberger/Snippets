17663813c0b21e65f3c0ae1f2f19b6fa998e14f0
jelllea <lea.jell@hs-augsburg.de>
2021-05-30 12:40:31 +0200

    reformatted folder db

--- backend/express_server/src/db/accounts.js
+++ backend/express_server/src/db/accounts.js
@@ -1,118 +1,98 @@
 /* template from https://gitlab.multimedia.hs-augsburg.de/kowa/wk_account_express_01a */
 
-import { query }  from './index.js'
+import { query } from "./index.js";
 
-const
-  getAccountsAll =
-    async () => 
-    { return [
-        200,
-        (await query
-         (`SELECT id, username, profile_picture 
+const getAccountsAll = async () => {
+    return [
+      200,
+      (
+        await query(`SELECT id, username, profile_picture 
            FROM   v_account
-          `
-         )
-        ).rows
-      ]  
-    },
+          `)
+      ).rows,
+    ];
+  },
 
-  getAccountSearch =
-  async (key) => 
-  { const result = 
-    await query
-    ( `SELECT id, username, profile_picture
+  getAccountSearch = async (key) => {
+    const result = await query(
+      `SELECT id, username, profile_picture
        FROM   v_account
        WHERE  username = $1::VARCHAR
-      `, 
+      `,
       [key]
     );
-  
-    return (result.rows.length === 0)
-           ? [404, {}]
-           : [200, result.rows[0]] 
+    return result.rows.length === 0 ? [404, {}] : [200, result.rows[0]];
   },
 
-  getAccounts =
-  async (key) => 
-  { return key ? getAccountSearch(key) : getAccountsAll() },
+  getAccounts = async (key) => {
+    return key ? getAccountSearch(key) : getAccountsAll();
+  },
 
-  getAccount =
-  async (id) => 
-  { const result = 
-    await query
-    ( `SELECT id, username, profile_picture
+  getAccount = async (id) => {
+    const result = await query(
+      `SELECT id, username, profile_picture
        FROM   v_account
        WHERE  $1::UUID = id
-      `, 
+      `,
       [id]
     );
-
-    return (result.rows.length === 0)
-           ? [404, {}]
-           : [200, result.rows[0]] 
+    return result.rows.length === 0 ? [404, {}] : [200, result.rows[0]];
   },
-  
+
   //TODO: check profile picture for accepted content? NOT $3::VARCHAR
-  postAccount =
-  async ({username, password, profile_picture}) => 
-  { const result = 
-    await query
-    ( `INSERT INTO v_account(username, password, profile_picture)
+  postAccount = async ({ username, password, profile_picture }) => {
+    const result = await query(
+      `INSERT INTO v_account(username, password, profile_picture)
        VALUES($1::VARCHAR, $2::VARCHAR, $3::BYTEA)
        RETURNING id
-      `, 
+      `,
       [username, password, profile_picture]
     );
-
-    return [201, result.rows[0]?.id]
+    return [201, result.rows[0]?.id];
   },
-  
+
   //TODO: check profile_picture for accepted content
-  putAccount =
-  async (id, {username, password, profile_picture}) => 
-  { await query
-    ( `UPDATE account
+  putAccount = async (id, { username, password, profile_picture }) => {
+    await query(
+      `UPDATE account
        SET username        = $2::VARCHAR,
            profile_picture = $3::BYTEA
        WHERE id = $1::UUID
-      `, 
+      `,
       [id, username, profile_picture]
     );
-    return 200
+    return 200;
   },
 
-  patchAccount =
-  async (id, {username, password, profile_picture}) => 
-  { await query
-    ( `UPDATE account
+  patchAccount = async (id, { username, password, profile_picture }) => {
+    await query(
+      `UPDATE account
        SET username        = COALESCE($2::VARCHAR, username),
            profile_picture = COALSECE($3::BYTEA, profile_picture)
        WHERE id = $1::UUID
-      `, 
+      `,
       [id, username, profile_picture]
     );
-    return 200
+    return 200;
   },
-
-  deleteAccount =
-  async (id) => 
-  { const result = 
-    await query
-    ( `DELETE 
+  
+  deleteAccount = async (id) => {
+    const result = await query(
+      `DELETE 
        FROM  v_account
        WHERE id = $1::UUID
        RETURNING *
-      `, 
+      `,
       [id]
     );
-    return result.rows[0] ? 200 : 204
+    return result.rows[0] ? 200 : 204;
   };
 
-export { 
+export {
   getAccounts,
   postAccount,
   getAccount,
   putAccount,
   patchAccount,
   deleteAccount,
-}
+};
--- backend/express_server/src/db/auth.js
+++ backend/express_server/src/db/auth.js
@@ -1,27 +1,19 @@
 /* template from https://gitlab.multimedia.hs-augsburg.de/kowa/wk_account_express_01a */
 
-import { query }  from './index.js'
+import { query } from "./index.js";
 
-const
-  postLogin =
-  async ({user, password}) => 
-  { const result = 
-    (await query
-     ( `SELECT check_password($1::VARCHAR, $2::VARCHAR) AS authorized, 
+const postLogin = async ({ user, password }) => {
+  const result = (
+    await query(
+      `SELECT check_password($1::VARCHAR, $2::VARCHAR) AS authorized, 
                id
         FROM   account
         WHERE  username = $1::VARCHAR
-       `, 
-       [user, password]
-     )
-    ).rows[0];
+       `,
+      [user, password]
+    )
+  ).rows[0];
+  return result ? [result.authorized ? 200 : 401, result.id] : [401, ""];
+};
 
-    return result 
-           ? [result.authorized ? 200 : 401, result.id] 
-           : [401, '']
-  };
-
-
-export default
-{ postLogin,
-}
+export default { postLogin };
