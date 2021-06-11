91f82fbad2d9ab03a00e36ef1e58d12403b605cd
jelllea <lea.jell@hs-augsburg.de>
2021-06-11 22:48:44 +0200

    added functions isAuthorized and isNotAuthorized

---
+++ backend/express_server/src/util/auth.js
@@ -0,0 +1,48 @@
+/* template: https://gitlab.multimedia.hs-augsburg.de/kowa/wk_account_express_01 */
+
+// see https://www.digitalocean.com/community/tutorials/nodejs-jwt-expressjs
+import jwt from 'jsonwebtoken'
+
+const
+{ TOKEN_SECRET = 'Please set the secret value in .env: TOKEN_SECRET=...' }
+  = process.env;
+
+function isNotAuthorized(req, res, next) 
+{ if (req.headers['authorization'] == null) 
+  { next() }
+  else 
+  { return res.status(400).json({message: 'already logged in'}) }
+}
+
+function isAuthorized(req, res, next) 
+{ const c_auth_header = req.headers['authorization']
+  const c_token       = c_auth_header && c_auth_header.split(' ')[1]
+
+  if (c_token == null) 
+  { return res.status(401).json({message: 'not logged in'}) }
+
+  jwt.verify
+  ( c_token, 
+    TOKEN_SECRET, 
+    (err, payload) => 
+    { //console.log(err, payload)
+      if (err) 
+      { return res.status(401).json({message: 'not logged in'}) }
+
+      req.user    = payload.user;
+      req.id      = payload.id;
+      req.isAdmin = payload.isAdmin;
+      next();
+    }
+  )
+}
+
+export
+{ isAuthorized,
+  isNotAuthorized,
+}
+
+export default
+{ isAuthorized,
+  isNotAuthorized,
+}
