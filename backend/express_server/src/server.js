0a901df4ff9dc3feaa650fcf8c737c644430f625
jelllea <lea.jell@hs-augsburg.de>
2021-06-14 15:15:44 +0200

    added routes for posts and comments

--- backend/express_server/src/server.js
+++ backend/express_server/src/server.js
@@ -5,9 +5,12 @@ import express from "express";
 import cors from "cors";
 
 import * as uuid from "uuid";
-//import auth      from './routes/auth.js'
+
 import { auth } from "./routes/auth.js";
 import { accounts } from "./routes/accounts.js";
+import { posts } from "./routes/posts.js";
+import { comments } from "./routes(comments.js";
+
 import { ValidationError } from "express-json-validator-middleware";
 
 const { PORT = 3000, SERVER = `http://localhost:${PORT}` } = process.env;
@@ -16,6 +19,8 @@ const c_uuid = uuid.v4;
 
 c_app.use('/v1',          auth);
 c_app.use("/v1/accounts", accounts);
+c_app.use("/v1/posts", posts);
+c_app.use("/v1/comments", comments);
 
 c_app.get("/v1/uuid", (req, res) => res.status(200).json(c_uuid()));
 
