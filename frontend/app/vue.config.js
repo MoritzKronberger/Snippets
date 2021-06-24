738b41cc081108bea80990c59e6ac4366bf579af
jelllea <lea.jell@hs-augsburg.de>
2021-06-24 13:47:31 +0200

    added navigation guards authorized and notAuthorized

--- frontend/app/vue.config.js
+++ frontend/app/vue.config.js
@@ -1,20 +1,39 @@
-module.exports = 
-{ css: 
-  { loaderOptions: 
-    { scss: 
-      { prependData: `@import '@/common/css/_config.scss';`,
+const path = require("path");
+
+function absolutePath(l_path) {
+  return path.resolve(__dirname, l_path);
+}
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      scss: { prependData: `@import '@/common/css/_config.scss';` },
+    },
+  },
+
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "/components": absolutePath("src/components/"),
+        "/router": absolutePath("src/router/"),
+        "/store": absolutePath("src/store/"),
+        "/views": absolutePath("src/views/"),
+        "/css": absolutePath("src/common/css/"),
+        "/img": absolutePath("src/common/img/"),
+        "/js": absolutePath("src/common/js/"),
+        "/json": absolutePath("src/common/json/"),
       },
     },
   },
 
-  devServer: 
-  { proxy: 
-    { "^/api/": 
-      { target: "http://localhost:3000",
+  devServer: {
+    proxy: {
+      "^/api/": {
+        target: "http://localhost:3000",
         pathRewrite: { "/api/": "/v1/" },
         changeOrigin: true,
-      //logLevel: "debug"
-      }
-    }
+        //logLevel: "debug"
+      },
+    },
   },
 };
