3cfea867b40666e1cb84fa0c5e30ec9c2ca5bfe3
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2021-06-30 18:22:42 +0200

    Add postgres query for trigram search

--- backend/express_server/src/db/posts.js
+++ backend/express_server/src/db/posts.js
@@ -9,20 +9,36 @@ const getPostsAll = async () => {
     return { status: 200, result: result.rows };
   },
   // TODO: weniger hacky implementieren?
-  getPostSorted = async (key) => {
+  getPostSorted = async (key, query_string) => {
     const view = await query(
       `SELECT view_name
        FROM get_sort_by_view_name
-       WHERE view_name = $1::UUID
+       WHERE id = $1::UUID
        `, 
       [key]
     );
-    const result = await query(
-      `SELECT sort_rank, id, creation_time, title, content, language, user_id, username, profile_picture, num_likes, num_comments, categories
-       FROM $1
-      `, 
-      [view.rows[0]]
-    );
+    if(view.rows.length===0){
+      return { status: 404, result: {} }
+    }
+    if(query_string){
+      const result = await query(
+        `SELECT DISTINCT sort_rank, p.id, creation_time, title, content, language, user_id, username, profile_picture, num_likes, num_comments, categories
+         FROM $1 p
+              JOIN has_category hc ON p.id = hc.post_id
+              JOIN e_category ct   ON hc.category_id = ct.id
+         WHERE trigram_category ILIKE '%' || $2::VARCHAR || '%' OR $2::VARCHAR <<% trigram_category
+        `, 
+        [view.rows[0], query_string]
+      );
+    }else{
+      const result = await query(
+        `SELECT sort_rank, id, creation_time, title, content, language, user_id, username, profile_picture, num_likes, num_comments, categories
+         FROM $1
+        `, 
+        [view.rows[0]]
+      );
+    }
+
     return result.rows.length === 0
       ? { status: 404, result: {} }
       : { status: 200, result: result.rows };
@@ -42,11 +58,11 @@ const getPostsAll = async () => {
       ? { status: 404, result: {} }
       : { status: 200, result: result.rows[0] };
   },
-  getPostsSearch = async (data) => {
+  getPostsSearch = async (key) => {
     const result = await query(
-      `
+      `SELECT 
       `, 
-      [data]
+      [key]
     );
     return result.rows;
   },
