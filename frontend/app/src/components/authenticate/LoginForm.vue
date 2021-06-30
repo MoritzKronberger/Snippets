a32cf6203c1acfc0653edc2c8680db3ea00aeded
jelllea <lea.jell@hs-augsburg.de>
2021-06-30 14:02:59 +0200

    added function for login

--- frontend/app/src/components/authenticate/LoginForm.vue
+++ frontend/app/src/components/authenticate/LoginForm.vue
@@ -3,7 +3,7 @@
     <h1>Login</h1>
     <div class="content auth">
       <Form auth_type="username_password" :auth="user" />
-      <Validation :object="user" button_name="Login" @click= "login" btn_class="medium"/>
+      <Validation :object="user" button_name="Login" @click= "doLogin" btn_class="medium"/>
       <p>No Account?</p>
       <router-link to="/register">
         <Button label="Register here" btn_class="medium"/>
@@ -24,18 +24,26 @@ export default {
   components: { Button, Form, Validation },
   computed: {
     ...mapFields("auth", ["user"]),
-    ...mapState('auth', ['success']),
-    error() { return this.success === false }
+    //...mapState('auth', ['success']),
+    //error() { return this.success === false }
   },
   methods: {
-  ...mapActions('auth', ['login']),
-   },
+    ...mapActions(['login']),
+
+    async doLogin() {
+      this.error = false;
+      const success = await this.login();
+      this.error = !success;
+      if (success)
+      { router.push('/') }
+    }
+  },
  
-   watch: { success(p_new)
+  /* watch: { success(p_new)
      {  if (p_new === true)
         { router.push('/') }
      }
-   },
+   },*/
 };
 </script>
 <style lang="scss"></style>
