3f78a30d3a9cdb596e36e6acc75ba9316e03f745
jelllea <lea.jell@hs-augsburg.de>
2021-07-01 14:40:11 +0200

    updated header

--- frontend/app/src/common/js/service/rest.js
+++ frontend/app/src/common/js/service/rest.js
@@ -8,13 +8,17 @@ const options = (method, token, url, data = null) => {
       data: data,
       validateStatus: false,
       headers: {
-        Accept: "application/json", // 'application/vnd.api+json'
-        ...(token ? { Authorization: `Bearer ${token}` } : {}),
+        'Accept': "application/json", // 'application/vnd.api+json'
+        ...(token ? { 'Authorization': `Bearer ${token}` } : {}),
       },
     };
   },
   json = async (method, token, url, data = null) => {
+    console.log("rest api", token);
+    console.log("rest axios", options(method, token, url, data));
     const res = await axios(options(method, token, url, data));
+    console.log("res token", res.headers.authorization);
+    console.log("res", res);
     return {
       status: res.status,
       headers: res.headers,
