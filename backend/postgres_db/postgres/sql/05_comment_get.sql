22c28ac6c0b011094bbd10c091eb67360fd212a7
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2021-07-03 09:25:53 +0200

    minor changes

--- backend/postgres_db/postgres/sql/05_comment_get.sql
+++ backend/postgres_db/postgres/sql/05_comment_get.sql
@@ -17,12 +17,11 @@ FROM comment
 
 CREATE VIEW get_full_comment (id, creation_time, content, post_id, user_id, username, num_likes)
 AS
-SELECT c.id, c.creation_time, c.content, p.id AS post_id, a.id AS user_id, a.username, COUNT(lk.id) AS num_likes
+SELECT c.id, c.creation_time, c.content, c.post_id, a.id AS user_id, a.username, COUNT(lk.id) AS num_likes
 FROM comment c
-     JOIN post p ON c.post_id = p.id 
      JOIN account a ON c.user_id = a.id
      LEFT JOIN user_like lk ON c.id = lk.comment_id
-GROUP BY c.id, p.id, a.id
+GROUP BY c.id, a.id
 ;
 
 COMMIT;
