dedc25003a8f3fc8f0ec52dd47fbd58e202d3007
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2021-07-04 19:25:18 +0200

    added filter buttons for getSortings

--- frontend/app/src/components/feed/search/Filter.vue
+++ frontend/app/src/components/feed/search/Filter.vue
@@ -1,15 +1,13 @@
 <template>
     <div class="center">
-    <Button label="Most Visited" btn_class="small" />
-    <Button label="Recently" btn_class="small" />
-    <Button label="Popular" btn_class="small" />
-    <Button label="Discussed" btn_class="small" />
+    <Button :label="filter.sort_by" btn_class="small" />
     </div>
 </template>
 <script>
 import Button from "../../Button.vue";
 export default {
   name: "Filter",
+    props: { filter: Object },
     components: { Button },
 };
 </script> 
