1385c932a0067d172901aece000ee4f85eee77e3
jelllea <lea.jell@hs-augsburg.de>
2021-07-06 18:48:13 +0200

    dispatch functions from login to feed

--- frontend/app/src/views/Feed.vue
+++ frontend/app/src/views/Feed.vue
@@ -47,19 +47,10 @@ export default {
   },
 
   beforeMount: function() {
-    //TODO: reactivate after login works
-    return this.$store
-      .dispatch("post/getLanguages", null, { root: true })
-      .then(() => {
-        return this.$store
-          .dispatch("post/getSortings", null, { root: true })
-          .then(() => {
-            return this.$store
-              .dispatch("post/getPosts", null, { root: true })
-              .then(() => {
-                return this.$store
-                  .dispatch("post/getComments", null, { root: true })
-                  .then(() => {
+    return this.$store.dispatch("post/getLanguages", null, { root: true }).then(() => {
+        return this.$store.dispatch("post/getSortings", null, { root: true }).then(() => {
+            return this.$store.dispatch("post/getPosts", null, { root: true }).then(() => {
+                return this.$store.dispatch("post/getComments", null, { root: true }).then(() => {
                     return this.$store.dispatch("post/getLikes", null, {
                       root: true,
                     });
