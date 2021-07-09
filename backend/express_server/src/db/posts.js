b8d2367573012e3dfd92f0b184b6775b58b2cf7d
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2021-07-09 15:18:44 +0200

    Update category_join_post to include trigram index

--- backend/express_server/src/db/posts.js
+++ backend/express_server/src/db/posts.js
@@ -22,26 +22,22 @@ const getPostsAll = async () => {
     let result = null;
     if(query_string){
       result = await query(
-        // TODO: use get_category_join_post instead of two joins
-        
         // allows for search queries where category names matching "delete" in a "keep - delete"-query-string will be excluded
         // regular expressions from:
         // https://stackoverflow.com/questions/4058923/how-can-i-use-regex-to-get-all-the-characters-after-a-specific-character-e-g-c/4059018
         `SELECT DISTINCT "sort_rank", p."id", "creation_time", "title", "content", "language", "user_id", "username", "num_likes", "num_comments", "categories"
          FROM ${view.rows[0].view_name} p
-              JOIN has_category hc ON p."id" = hc."post_id"
-              JOIN e_category ct   ON hc."category_id" = ct."id"
+              JOIN get_category_join_post cjp ON p."id" = cjp."post_id"
          WHERE "trigram_category" ILIKE '%' || 
-                                        (SELECT ct2."name" FROM e_category ct2 WHERE (SELECT TRIM(regexp_replace(($1::VARCHAR), '-.*$', ''))) <<% "trigram_category")
+                                        (SELECT ct."name" FROM e_category ct WHERE (SELECT TRIM(regexp_replace(($1::VARCHAR), '-.*$', ''))) <<% "trigram_category")
                                         || '%'
          
          EXCEPT
 
          SELECT DISTINCT "sort_rank", p."id", "creation_time", "title", "content", "language", "user_id", "username", "num_likes", "num_comments", "categories"
          FROM ${view.rows[0].view_name} p
-              JOIN has_category hc ON p."id" = hc."post_id"
-              JOIN e_category ct   ON hc."category_id" = ct."id"
-         WHERE ct."name" = (SELECT TRIM((regexp_matches(($1::VARCHAR), '[^-]*$'))[1]))
+              JOIN get_category_join_post cjp ON p."id" = cjp."post_id"
+         WHERE cjp."name" = (SELECT TRIM((regexp_matches(($1::VARCHAR), '[^-]*$'))[1]))
          ;
         `, 
         [query_string]
--- backend/postgres_db/postgres/sql/03_category_get.sql
+++ backend/postgres_db/postgres/sql/03_category_get.sql
@@ -15,9 +15,9 @@ SELECT "id", "name"
 FROM   e_category
 ;
 
-CREATE VIEW get_category_join_post ("id", "name", "post_id")
+CREATE VIEW get_category_join_post ("id", "name", "trigram_category", "post_id")
 AS
-SELECT c."id", c."name", hc."post_id"
+SELECT c."id", c."name", c."trigram_category", hc."post_id"
 FROM   e_category c
        JOIN has_category hc ON c."id" = hc."category_id"
 ;
