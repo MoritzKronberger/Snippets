e45141d1d3ec8f4fa7f33c8b5514cd575f1aec2d
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2021-07-09 10:33:04 +0200

    Update search function to allow exclusions

--- backend/express_server/src/db/posts.js
+++ backend/express_server/src/db/posts.js
@@ -8,7 +8,6 @@ const getPostsAll = async () => {
       `);
     return { status: 200, result: result.rows };
   },
-  // TODO: weniger hacky implementieren?
   getPostSorted = async (key, query_string) => {
     const view = await query(
       `SELECT "view_name"
@@ -23,11 +22,25 @@ const getPostsAll = async () => {
     let result = null;
     if(query_string){
       result = await query(
-        `SELECT DISTINCT "sort_rank", "p.id", "creation_time", "title", "content", "language", "user_id", "username", "num_likes", "num_comments", "categories"
+        // allows for search queries where category names matching "delete" in a "keep - delete"-query-string will be excluded
+        // regular expressions from:
+        // https://stackoverflow.com/questions/4058923/how-can-i-use-regex-to-get-all-the-characters-after-a-specific-character-e-g-c/4059018
+        `SELECT DISTINCT "sort_rank", p."id", "creation_time", "title", "content", "language", "user_id", "username", "num_likes", "num_comments", "categories"
          FROM ${view.rows[0].view_name} p
-              JOIN has_category hc ON "p.id" = hc."post_id"
+              JOIN has_category hc ON p."id" = hc."post_id"
               JOIN e_category ct   ON hc."category_id" = ct."id"
-         WHERE "trigram_category" ILIKE '%' || $1::VARCHAR || '%' OR $1::VARCHAR <<% "trigram_category"
+         WHERE "trigram_category" ILIKE '%' || 
+                                        (SELECT ct2."name" FROM e_category ct2 WHERE (SELECT TRIM(regexp_replace(($1::VARCHAR), '-.*$', ''))) <<% "trigram_category")
+                                        || '%'
+         
+         EXCEPT
+
+         SELECT DISTINCT "sort_rank", p."id", "creation_time", "title", "content", "language", "user_id", "username", "num_likes", "num_comments", "categories"
+         FROM ${view.rows[0].view_name} p
+              JOIN has_category hc ON p."id" = hc."post_id"
+              JOIN e_category ct   ON hc."category_id" = ct."id"
+         WHERE ct."name" = (SELECT TRIM((regexp_matches(($1::VARCHAR), '[^-]*$'))[1]))
+         ;
         `, 
         [query_string]
       );
