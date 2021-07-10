34f91d6abbe74111e1cc2b91866b1a6cdd90a92d
jelllea <lea.jell@hs-augsburg.de>
2021-07-10 16:57:07 +0200

    reformatted schema files

--- backend/express_server/src/schema/post-schema.js
+++ backend/express_server/src/schema/post-schema.js
@@ -2,12 +2,12 @@ const postSchema = {
   type: "object",
   properties: {
     id: { type: ["string", "null"], format: "uuid" },
-    creation_time: { type: ["string", "null"], format:"date-time" },
+    creation_time: { type: ["string", "null"], format: "date-time" },
     title: { type: ["string", "null"] },
     content: { type: ["string", "null"] },
     language_id: { type: ["string", "null"], format: "uuid" },
     user_id: { type: "string", format: "uuid" },
-    category: { type: ["array", "string", "null"] }
+    category: { type: ["array", "string", "null"] },
   },
 };
 
--- backend/express_server/src/schema/userLike-schema.js
+++ backend/express_server/src/schema/userLike-schema.js
@@ -1,12 +1,12 @@
 const userLikeSchema = {
-    type: "object",
-    properties: {
-        id: { type: "string", format: "uuid"},
-        user_id: { type: ["string", "null"], format: "uuid" },
-        post_id: { type: ["string", "null"], format: "uuid" },
-        comment_id: { type: ["string", "null"], format: "uuid" },
-        subject_id: { type: ["string", "null"], format: "uuid" }
-    },
+  type: "object",
+  properties: {
+    id: { type: "string", format: "uuid" },
+    user_id: { type: ["string", "null"], format: "uuid" },
+    post_id: { type: ["string", "null"], format: "uuid" },
+    comment_id: { type: ["string", "null"], format: "uuid" },
+    subject_id: { type: ["string", "null"], format: "uuid" },
+  },
 };
 
 export default userLikeSchema;
