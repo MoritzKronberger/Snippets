f0c43a42542dd1342a581056ec5008b36931499d
Moritz Kronberger <moritz.kronberger@hs-augsburg.de>
2021-07-11 10:08:33 +0200

    Update search function and cleanup

--- backend/express_server/src/db/posts.js
+++ backend/express_server/src/db/posts.js
@@ -3,16 +3,20 @@ import { query } from "./index.js";
 // TODO: limit für returned rows?
 const getPostsAll = async () => {
     const result = await query(
-      `SELECT "id", "creation_time", "title", "content", "language", "user_id", "username", "num_likes", "num_comments", "categories"
-       FROM get_full_post
+      `SELECT "id", "creation_time", "title", "content", 
+              "language", 
+              "user_id", "username", 
+              "num_likes", "num_comments", 
+              "categories"
+       FROM   get_full_post
       `);
     return { status: 200, result: result.rows };
   },
   getPostSorted = async (key, query_string) => {
     const view = await query(
       `SELECT "view_name"
-       FROM get_sort_by_view_name
-       WHERE "id" = $1::UUID
+       FROM   get_sort_by_view_name
+       WHERE  "id" = $1::UUID
        `, 
       [key]
     );
@@ -23,27 +27,48 @@ const getPostsAll = async () => {
     if(query_string){
       result = await query(
         // allows for search queries where category names matching "delete" in a "keep - delete"-query_string will be excluded
-        `SELECT DISTINCT "sort_rank", p."id", "creation_time", "title", "content", "language", "user_id", "username", "num_likes", "num_comments", "categories"
-         FROM ${view.rows[0].view_name} p
-              JOIN get_category_join_post cjp ON p."id" = cjp."post_id"
+        `SELECT DISTINCT "sort_rank", 
+                         p."id", "creation_time", "title", "content", 
+                         "language", 
+                         "user_id", "username", 
+                         "num_likes", "num_comments", 
+                         "categories"
+         FROM  ${view.rows[0].view_name} p
+               JOIN get_category_join_post cjp ON p."id" = cjp."post_id"
          WHERE "trigram_category" ILIKE '%' || 
-                                        (SELECT ct."name" FROM e_category ct WHERE (SELECT TRIM(SPLIT_PART(($1::VARCHAR), '-', 1))) <<% "trigram_category")
+                                        COALESCE(
+                                         (SELECT ct."name" 
+                                          FROM   e_category ct 
+                                          WHERE  (SELECT TRIM(SPLIT_PART($1::VARCHAR, '-', 1))) <<% "trigram_category"
+                                         ),
+                                         (SELECT TRIM(SPLIT_PART($1::VARCHAR, '-', 1)))
+                                        )
                                         || '%'
          
          EXCEPT
 
-         SELECT DISTINCT "sort_rank", p."id", "creation_time", "title", "content", "language", "user_id", "username", "num_likes", "num_comments", "categories"
-         FROM ${view.rows[0].view_name} p
-              JOIN get_category_join_post cjp ON p."id" = cjp."post_id"
-         WHERE cjp."name" = (SELECT TRIM(SPLIT_PART(($1::VARCHAR), '-', 2)))
+         SELECT DISTINCT "sort_rank", 
+                         p."id", "creation_time", "title", "content", 
+                         "language", 
+                         "user_id", "username", 
+                         "num_likes", "num_comments", 
+                         "categories"
+         FROM  ${view.rows[0].view_name} p
+               JOIN get_category_join_post cjp ON p."id" = cjp."post_id"
+         WHERE cjp."name" = (SELECT TRIM(SPLIT_PART($1::VARCHAR, '-', 2)))
          ;
         `, 
         [query_string]
       );
     } else {
       result = await query(
-        `SELECT "sort_rank", "id", "creation_time", "title", "content", "language", "user_id", "username", "num_likes", "num_comments", "categories"
-         FROM ${view.rows[0].view_name}
+        `SELECT "sort_rank", 
+                "id", "creation_time", "title", "content", 
+                "language", 
+                "user_id", "username", 
+                "num_likes", "num_comments", 
+                "categories"
+         FROM   ${view.rows[0].view_name}
         `
       );
     }
@@ -58,8 +83,8 @@ const getPostsAll = async () => {
   getPost = async (id) => {
     const result = await query(
       `SELECT "id", "user_id"
-       FROM get_post
-       WHERE "id" = $1::UUID
+       FROM   get_post
+       WHERE  "id" = $1::UUID
       `, 
       [id]
     );
@@ -70,8 +95,8 @@ const getPostsAll = async () => {
   getPostsWithCategories = async (id) => {
     const result = await query(
       `SELECT "id" AS "category_id", "name", "post_id"
-       FROM get_category_join_post
-       WHERE "post_id" = $1::UUID
+       FROM   get_category_join_post
+       WHERE  "post_id" = $1::UUID
       `, 
       [id]
     );
